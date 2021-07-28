2d63306de69e504d47860655b8f67eb422f5fa87
hyeongwoo-Lee <lhw970804@naver.com>
2021-07-29 02:04:49 +0900

    14강

--- src/main/java/com/cos/blog/controller/blogControllerTest.java
+++ src/main/java/com/cos/blog/controller/blogControllerTest.java
@@ -17,6 +17,4 @@ public class blogControllerTest {
         return "git";
     }
 
-
-
 }
---
+++ src/main/java/com/cos/blog/test/Member.java
@@ -0,0 +1,27 @@
+package com.cos.blog.test;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    //private 함수에 다이렉트로 접근하는 것을 막음 -> public 메서드를 작성해 접근
+
+    private int id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+
+    public Member(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
